65ba6c588e119c778ae40a4c3fdd7aaf422b1e62
GABRIEL DE LIMA SOUZA MARCILIO <gabriel.marcilio@etec.sp.gov.br>
2020-11-11 14:29:33 -0300

    Armazenando direções

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import React, {useState, useEffect} from 'react'
+import mov from './config/mov'
+
 const App = () => {
+
+  const [posicao, setPosicao] = useState();
+  const [direcao, setDirecao] = useState();
+
+  useEffect(()=>{
+    document.querySelector("body").addEventListener("keyup", toggleCommand);
+  }, [])
+
+  const toggleCommand = (e) =>{
+    atualizarDirecao(e.keyCode)
+  }
+
+  const atualizarDirecao = (e) =>{
+    if(mov.CIMA.PADRAO.includes(e)){
+      setDirecao(mov.CIMA.VALOR);
+    }
+    else if(mov.BAIXO.PADRAO.includes(e)){
+      setDirecao(mov.BAIXO.VALOR);
+    }
+    else if(mov.ESQUERDA.PADRAO.includes(e)){
+      setDirecao(mov.ESQUERDA.VALOR);
+    }
+    else {
+      setDirecao(mov.DIREITA.VALOR);
+    }
+  }
+
   return (
     <div className="App">
+      <a>{direcao}</a>
     </div>
   );
 }
---
+++ src/config/mov.js
@@ -0,0 +1,18 @@
+export default {
+    CIMA: {
+        PADRAO: [87,38], //vai virar numero,
+        VALOR: "W",
+    },
+    BAIXO: {
+        PADRAO: [83,40], //vai virar numero,
+        VALOR: "S",
+    },
+    ESQUERDA: {
+        PADRAO: [65,37], //vai virar numero,
+        VALOR: "A",
+    },
+    DIREITA: {
+        PADRAO: [68,39], //vai virar numero,
+        VALOR: "D",
+    },
+}
