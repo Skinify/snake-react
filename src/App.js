38bac7670507824fbec3c00fb9264eb338eb4843
Skinify <gabrielZK2000@hotmail.com>
2020-11-14 00:58:06 -0300

    Gerando comida

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import mov from './config/mov'
 import mapa from './config/mapa'
+import inteiroAleatorio from '../src/helper/inteiroAleatorio'
 
 const App = () => {
   const [tempo, setTempo] = useState(0);
@@ -11,14 +12,20 @@ const App = () => {
   })
   const [direcao, setDirecao] = useState(mov.DIREITA.VALOR);
 
+  const [comida,setComida] = useState({
+    X:50,
+    Y:120,
+  });
+
   const body = document.querySelector("body");
 
   useEffect(()=>{
     body.addEventListener("keyup", toggleCommand);
+    verificarColisao();
     const temporizador = setInterval(() => {
       setTempo(tempo => tempo + 1)
       andar();
-    }, 250);
+    }, 100);
     return () => {
       clearInterval(temporizador)
       body.removeEventListener("keyup", toggleCommand);
@@ -28,6 +35,32 @@ const App = () => {
 
   const toggleCommand = (e) =>{
     atualizarDirecao(e.keyCode)
+  }
+
+  const verificarColisao = () =>{
+    if(posicaoAtual.X === comida.X){
+      if(posicaoAtual.Y === comida.Y){
+        console.log("eae")
+        gerarComida();
+      }
+    }
+  }
+
+  const gerarComida = () => {
+      let x = 0,y = 0;
+    do{
+      x = inteiroAleatorio(0,10) * 10
+      y = inteiroAleatorio(0,10) * 10
+      if(posicaoAtual.X !== x){
+        if(posicaoAtual.Y !== y){
+          break; 
+        }
+      }
+    }while(true)
+    setComida({
+      X: x,
+      Y: y
+    })
   }
 
   const atualizarDirecao = (e) =>{
@@ -68,7 +101,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <a className="player" style={{position:"absolute",top:`${posicaoAtual.Y}px`,left:`${posicaoAtual.X}px`}}>x</a>
+      <a className="player" style={{position:"absolute",top:`${posicaoAtual.Y}px`,left:`${posicaoAtual.X}px`}}>X</a>
+        <a style={{position:"absolute",top:`${comida.Y}px`,left:`${comida.X}px`}}>B</a>
     </div>
   );
 }
